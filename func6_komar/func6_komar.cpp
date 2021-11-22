1c5b2e419e532ff711b17280748c80094a46d686
V1PeRI4 <V1PeRI4@DESKTOP-C59PJAE>
2021-11-22 22:29:07 +0300

    Добавьте файлы проекта.

---
+++ func6_komar/func6_komar.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+double celsToFaring(double);
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	double dist;
+	cout << "Введите расстояние в световых годах: ";
+	cin >> dist;
+	double distFin = celsToFaring(dist);
+	cout << dist << " световых лет - это " << distFin << " астрономических едениц";
+	return 0;
+}
+
+double celsToFaring(double sts)
+{
+	sts = sts * 63240;
+	return sts;
+}
